06043fbc3fb2ccc41839e8f8636a7f5c50b7ec60
Dean DeHart <dddehart@oakland.edu>
2016-11-14 23:48:33 -0500

    Fixed brief bug.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,7 +28,7 @@ class BlogsController < ApplicationController
     @blog.user_id = current_user.id
 
     respond_to do |format|
-      if @blog.save
+      if @blog.save!
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
         format.json { render :show, status: :created, location: @blog }
       else
@@ -70,6 +70,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body, :author)
+      params.require(:blog).permit(:title, :body, :user_id)
     end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,6 @@
 class Blog < ApplicationRecord
 	belongs_to :user
 	validates :title, :presence => true
-	validates :brief, :presence => true
 	validates :body,  :presence => true
 	self.per_page = 10
 end
