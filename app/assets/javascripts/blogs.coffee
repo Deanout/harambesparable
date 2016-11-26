b23ded1f87832480ab9253b51d557dc695d5e1c9
Deanout <dddehart@oakland.edu>
2016-11-26 22:53:41 +0000

    Test

--- app/assets/javascripts/blogs.coffee
+++ app/assets/javascripts/blogs.coffee
@@ -1,9 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready ->
-  $(document).click (e) ->
-    $('.cke_dialog_tab').css 'color': '#000'
-    return
-  $('img').css 'max-width': '100%'
-  return
--- public/assets/ckeditor/config.js
+++ public/assets/ckeditor/config.js
@@ -3,8 +3,10 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-};
+ CKEDITOR.editorConfig = function( config ) {
+ 	config.language = 'en';
+ 	config.uiColor = '#F7B42C';
+ 	config.height = 300;
+ 	config.toolbarCanCollapse = true;
+  config.allowedContent = true;
+ };
